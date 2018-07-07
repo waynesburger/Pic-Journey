63bade37780d2b27aebe6f283ab181b95ad0d594
waynesburger <freeholays@hotmail.com>
2018-07-07 13:08:35 -0700

    Add files via upload

---
+++ firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyD1Nu7AubLbsGLE47YqvO3D3mJgWHNZtVc",
+    authDomain: "journimg-1529547464712.firebaseapp.com",
+    databaseURL: "https://journimg-1529547464712.firebaseio.com",
+    projectId: "journimg-1529547464712",
+    storageBucket: "journimg-1529547464712.appspot.com",
+    messagingSenderId: "588638304922"
+}

16d274631afc1e64c559d801191f030091f6e1e7
Wayne Weidert <freeholays@hotmail.com>
2018-08-17 23:51:01 -0700

    Add files via upload

---
+++ databaseService.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+//import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Photo } from '../objects/photograph';
+import { UserInfo } from '../objects/userInfo';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import firebase from 'firebase';
+
+@Injectable()
+export class DataBaseService {
+
+}

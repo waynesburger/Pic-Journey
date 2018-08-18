a9810c6a939834d79afa4a16092116029f25a616
Wayne Weidert <freeholays@hotmail.com>
2018-08-17 23:52:11 -0700

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -9,34 +9,45 @@ import { MyApp } from './app.component';
 import { MapPage } from '../pages/mapPage/mapPage';
 import { AlbumPage } from '../pages/albumPage/albumPage';
 import { TakePhoto } from '../pages/cameraPage/cameraPage';
+import { ImagesPage } from '../pages/viewImagePage/viewImagePage';
 
 import { HttpModule } from '@angular/http';
 import { FIREBASE_CONFIG } from '../app/firebase.config';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { DataBaseService } from '../services/databaseService';
 
 @NgModule({
   declarations: [
     MyApp,
     TakePhoto,
     MapPage,
-    AlbumPage
+    AlbumPage,
+    ImagesPage
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule,
+    AngularFireStorageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     TakePhoto,
     MapPage,
-    AlbumPage
+    AlbumPage,
+    ImagesPage
   ],
   providers: [
     Geolocation,
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataBaseService
   ]
 })
 export class AppModule {}
